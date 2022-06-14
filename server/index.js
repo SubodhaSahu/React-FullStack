a0f878971a552c174e9b61d238fac1cb5fbbffca
Subodha Sahu <subodha.sahu@rapidfunnel.com>
2022-06-14 12:39:30 +0530

    Add comment section in the post

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 //Configuring the database
-const db = require('./models')
+const db = require("./models");
 
 //Routers
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
+const commentRouter = require("./routes/Comments");
+app.use("/comments", commentRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/test', (req, res) => {
-    res.send([1,2,3])
-})
-
-
+app.get("/test", (req, res) => {
+  res.send([1, 2, 3]);
+});
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log('Listening to port 3001...');
-    });
-})
+  app.listen(3001, () => {
+    console.log("Listening to port 3001...");
+  });
+});
